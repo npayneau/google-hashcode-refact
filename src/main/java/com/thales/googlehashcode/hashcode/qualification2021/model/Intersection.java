97912cc6198b07f9a71b89140d611be46d16063b
nicolas.payneau <nicolas.payneau@thalesgroup.com>
2021-02-25 20:08:00 +0100

    Ajout d'intersection

---
+++ src/main/java/com/thales/googlehashcode/hashcode/qualification2021/model/Intersection.java
@@ -0,0 +1,13 @@
+package com.thales.googlehashcode.hashcode.qualification2021.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.Map;
+
+@Builder
+@Data
+public class Intersection {
+
+    private Map<Street, Integer> incomingStreets;
+}
