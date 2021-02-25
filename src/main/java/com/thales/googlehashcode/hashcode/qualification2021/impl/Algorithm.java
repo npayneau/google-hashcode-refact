612f6ea7f81f81cdcfdeb97c4f4d61b2c667de27
nicolas.payneau <nicolas.payneau@thalesgroup.com>
2021-02-25 20:17:48 +0100

    Add outwritter

--- src/main/java/com/thales/googlehashcode/hashcode/qualification2021/impl/Algorithm.java
+++ src/main/java/com/thales/googlehashcode/hashcode/qualification2021/impl/Algorithm.java
@@ -4,20 +4,24 @@ import com.thales.googlehashcode.hashcode.common.HashCodeAlgorithm;
 import com.thales.googlehashcode.hashcode.common.HashCodeLogger;
 import com.thales.googlehashcode.hashcode.common.HashCodeSolution;
 import com.thales.googlehashcode.hashcode.qualification2021.model.InputValue;
+import com.thales.googlehashcode.hashcode.qualification2021.model.Intersection;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Algorithm implements HashCodeAlgorithm<InputValue> {
 
     @Override
     public HashCodeSolution run(InputValue input, HashCodeLogger logger) {
-
+        List<Intersection> intersections = new ArrayList<>();
         //create output
-      /*  List<String> out = new ArrayList<>();
-        out.add(String.valueOf(libraryOut.size()));
-        for (Library library : libraryOut) {
-            out.add(library.getId() + " " + library.getOutBooks().size());
-            out.add(library.getOutBooksString().substring(0, library.getOutBooksString().length() - 1));
-        }*/
-        //return new HashCodeSolution(out, 50);
-        return null;
+        List<String> out = new ArrayList<>();
+        out.add(String.valueOf(intersections.size()));
+        for (Intersection intersection : intersections) {
+            out.add(String.valueOf(intersection.getId()));
+            out.add(String.valueOf(intersection.getIncomingStreets().size()));
+            intersection.getIncomingStreets().forEach((street, time) -> out.add(street + " " + time));
+        }
+        return new HashCodeSolution(out, 50);
     }
 }
